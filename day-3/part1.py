914dc32b70c7588bb04bd7c4b1a10dc750f5aaba
Renan Vieira <renanvieira@live.com>
2018-12-16 14:27:22 -0200

    added day-3 solution

--- day-3/part1.py
+++ day-3/part1.py
@@ -1,5 +1,35 @@
+import re
+
+
+def create_claims(args):
+    claims = list()
+    for item in args:
+        matches = re.search(r'#(\d+) @ (\d+,\d+): (\d+x\d+)', item)
+        coords = matches.groups()[1].split(',')
+        size = matches.groups()[2].split('x')
+
+        claim = dict()
+        claim["id"] = matches.groups()[0]
+        claim["x"] = int(coords[0])
+        claim["y"] = int(coords[1])
+        claim["width"] = int(size[0])
+        claim["height"] = int(size[1])
+
+        claims.append(claim)
+    return claims
+
+
 def main(args):
-    pass
+    claims = create_claims(args)
+    claimed = set()
+    overlapping = set()
+
+    for claim in claims:
+        cur_claimed = set(
+            (i + claim["x"], claim["y"] + j) for i in range(claim["width"]) for j in range(claim["height"]))
+        overlapping |= cur_claimed & claimed
+        claimed |= cur_claimed
+    print(f"Number of overlapping claims: {len(overlapping)}")
 
 
 if __name__ == "__main__":
--- day-3/part2.py
+++ day-3/part2.py
@@ -1,5 +1,42 @@
+import re
+
+
+def create_claims(args):
+    claims = list()
+    for item in args:
+        matches = re.search(r'#(\d+) @ (\d+,\d+): (\d+x\d+)', item)
+        coords = matches.groups()[1].split(',')
+        size = matches.groups()[2].split('x')
+
+        claim = dict()
+        claim["id"] = matches.groups()[0]
+        claim["x"] = int(coords[0])
+        claim["y"] = int(coords[1])
+        claim["width"] = int(size[0])
+        claim["height"] = int(size[1])
+
+        claims.append(claim)
+    return claims
+
+
 def main(args):
-    pass
+    claims = create_claims(args)
+    claimed = set()
+    overlapping = set()
+
+    for claim in claims:
+        cur_claimed = set(
+            (i + claim["x"], claim["y"] + j) for i in range(claim["width"]) for j in range(claim["height"]))
+
+        overlapping |= cur_claimed & claimed
+        claimed |= cur_claimed
+
+    for claim in claims:
+        cur_claimed = set(
+            (i + claim["x"], claim["y"] + j) for i in range(claim["width"]) for j in range(claim["height"]))
+
+        if not cur_claimed & overlapping:
+            print(f"This claim don't overlap anything: {claim.get('id')}")
 
 
 if __name__ == "__main__":
